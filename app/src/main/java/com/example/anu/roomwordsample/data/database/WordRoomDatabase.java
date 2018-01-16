7cfa81627439ac717e61eb02bf2a52e58ea7b6df
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-16 23:01:38 +0530

    Add Room database

---
+++ app/src/main/java/com/example/anu/roomwordsample/data/database/WordRoomDatabase.java
@@ -0,0 +1,34 @@
+package com.example.anu.roomwordsample.data.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.anu.roomwordsample.data.Word;
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    private WordRoomDatabase newInstance;
+
+    //abstract getter method for the dao
+    public abstract WordDao getWordDao();
+
+    /**
+     * Make the WordRoomDatabase a singleton to prevent having multiple instances of the database opened at the same time.
+     * @return
+     */
+    public WordRoomDatabase getNewInstance(Context context){
+        if (null == newInstance){
+            synchronized (WordRoomDatabase.class){
+                if (null == newInstance){
+                    //create database
+                    newInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database").build();
+                }
+            }
+        }
+        return newInstance;
+    }
+}
