eee2d08334854eddb7cec14f384fe3341668aeaa
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-16 22:31:24 +0530

    Add entity

---
+++ app/src/main/java/com/example/anu/roomwordsample/entity/Word.java
@@ -0,0 +1,30 @@
+package com.example.anu.roomwordsample.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    /**
+     * word act as it's own primary key
+     * it cannot be null
+     * @param mWord
+     */
+    public Word(String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getmWord() {
+        return mWord;
+    }
+}
