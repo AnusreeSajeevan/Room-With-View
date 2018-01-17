ebd626008fdbabd6f423ae5532439e93c5fb59b0
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-17 20:53:40 +0530

    Populate database

--- app/src/main/java/com/example/anu/roomwordsample/data/database/WordRoomDatabase.java
+++ app/src/main/java/com/example/anu/roomwordsample/data/database/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.example.anu.roomwordsample.data.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import com.example.anu.roomwordsample.data.Word;
 
@@ -25,10 +28,57 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if (null == newInstance){
                     //create database
                     newInstance = Room.databaseBuilder(context.getApplicationContext(),
-                            WordRoomDatabase.class, "word_database").build();
+                            WordRoomDatabase.class, "word_database").addCallback(callback).build();
                 }
             }
         }
         return newInstance;
+    }
+
+    /**
+     * To delete all content and repopulate the database whenever the app is started, you create a RoomDatabase.Callback
+     * and override onOpen(). Because you cannot do Room database operations on the UI thread,
+     * onOpen() creates and executes an AsyncTask to add content to the database.
+     */
+    private static RoomDatabase.Callback callback = new Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new WordsInsertAsyncTask(newInstance).execute();
+        }
+    };
+
+    /**
+     * class which deletes the contents of the database, then populates it with new words
+     */
+    public static class WordsInsertAsyncTask extends AsyncTask<Void, Void, Void>{
+
+        private WordDao wordDao;
+
+        public WordsInsertAsyncTask(WordRoomDatabase database) {
+            this.wordDao = database.getWordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            wordDao.deleteAllWords();
+
+            Word word1 = new Word("Hai");
+            wordDao.insertWord(word1);
+
+            Word word2 = new Word("Hello");
+            wordDao.insertWord(word2);
+
+            Word word3 = new Word("How");
+            wordDao.insertWord(word3);
+
+            Word word4 = new Word("Are");
+            wordDao.insertWord(word4);
+
+            Word word5 = new Word("You?");
+            wordDao.insertWord(word5);
+
+            return null;
+        }
     }
 }
