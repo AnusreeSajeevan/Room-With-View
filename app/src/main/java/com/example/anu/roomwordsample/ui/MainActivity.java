76138848afd9392cb3a25a8d3d346030cbd9f1df
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-17 11:44:55 +0530

    Create view model

--- app/src/main/java/com/example/anu/roomwordsample/ui/MainActivity.java
+++ app/src/main/java/com/example/anu/roomwordsample/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.anu.roomwordsample;
+package com.example.anu.roomwordsample.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.anu.roomwordsample.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/anu/roomwordsample/ui/WordViewModel.java
@@ -0,0 +1,46 @@
+package com.example.anu.roomwordsample.ui;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.example.anu.roomwordsample.data.Word;
+import com.example.anu.roomwordsample.data.WordRepository;
+
+import java.util.List;
+
+/**
+ * Created by Design on 17-01-2018.
+ */
+
+public class WordViewModel extends AndroidViewModel{
+
+    private WordRepository wordRepository;
+
+    //variable to cache the list of words.
+    private LiveData<List<Word>> mWords;
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+        wordRepository = new WordRepository(application);
+        mWords = wordRepository.getAllWords();
+    }
+
+    /**
+     * getter for all the words. This completely hides the implementation from the UI.
+     * @return all the words
+     */
+    public LiveData<List<Word>> getAllWords() {
+        return mWords;
+    }
+
+    /**
+     * wrapper method insert() that calls the repository's insert() method.
+     * In this way, the implementation of insert() is completely hidden from the UI.
+     * @param word the word to be inserted
+     */
+    public void insert(Word word){
+        wordRepository.insertWord(word);
+    }
+}
